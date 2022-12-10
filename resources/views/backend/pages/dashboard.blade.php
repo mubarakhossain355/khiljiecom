285cbc813bf7edf75d8acbdd6bc7657355f52a99
mubarak_hossain <mubarakkha355@gmail.com>
2022-12-10 20:52:44 +0600

    backend mastering done

---
+++ resources/views/backend/pages/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('backend.layouts.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('admin.content')
+    <div class="row">
+        <div class="col-12">
+            Dashboard
+        </div>
+    </div>
+@endsection
